e92bdf0b2dfe186f666befdac09298c6a46b1af0
Vitalii Zakharov <zakharovvi@gmail.com>
2022-09-27 22:01:32 +0300

    fix rie integration test

--- tests/rie/rie_integration_test.go
+++ tests/rie/rie_integration_test.go
@@ -2,6 +2,7 @@ package main_test
 
 import (
 	"bytes"
+	"io"
 	"net/http"
 	"os"
 	"os/exec"
@@ -16,6 +17,15 @@ extension.HandleInvokeEvent
 
 func TestRIE(t *testing.T) {
 	rieCmd := exec.Command("/tmp/aws-lambda-rie", "./rie")
+	stdoutPipe, err := rieCmd.StdoutPipe()
+	if err != nil {
+		t.Log(err)
+	}
+	stderrPipe, err := rieCmd.StderrPipe()
+	if err != nil {
+		t.Log(err)
+	}
+
 	if err := rieCmd.Start(); err != nil {
 		t.Fatal(err)
 	}
@@ -23,15 +33,38 @@ func TestRIE(t *testing.T) {
 		if err := rieCmd.Process.Signal(os.Interrupt); err != nil {
 			t.Fatal(err)
 		}
+
+		stdout, err := io.ReadAll(stdoutPipe)
+		if err != nil {
+			t.Log(err)
+		}
+		t.Logf("stdout: %s", stdout)
+		stderr, err := io.ReadAll(stderrPipe)
+		if err != nil {
+			t.Log(err)
+		}
+		t.Logf("stderr: %s", stderr)
+
+		if err := rieCmd.Wait(); err != nil {
+			t.Log(err)
+		}
 	}()
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(2 * time.Second)
 
-	resp, err := http.Get("http://localhost:8080/2015-03-31/functions/function/invocation")
+	resp, err := http.Get("http://localhost:8080/2015-03-31/functions/function/invocations")
 	if err != nil {
 		t.Fatal(err)
 	}
 	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("rie response: %s", body)
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("want 200 OK HTTP status code. Got %d", resp.StatusCode)
+	}
 
 	// assert file
 	got, err := os.ReadFile("/tmp/rie-test-journal")
